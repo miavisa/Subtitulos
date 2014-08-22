af72d02dd56168d5d58b69ded61096de6dda9a65
Javier Alonso <arlekias@gmail.com>
2014-08-22 12:33:53 +0200

    Initial commit

---
+++ Subtitulos/Classes/Subtitulo.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Subtitulos.Classes
+{
+    public class Subtitulo
+    {
+        public string id;
+        public string currentEspisode;
+        public string show;
+        public string localLocation;
+
+        public bool SetId()
+        {
+            try
+            {
+                Console.Write("Getting Id -> ");
+                WebClient w = new WebClient();
+                string s = w.DownloadString("http://www.subtitulos.es/" + this.show + "/" + this.currentEspisode);
+                Match match = Regex.Match(s, @"subID='.....", RegexOptions.IgnoreCase);
+                string m = match.Groups[0].Value;
+                m = m.Replace("subID='", "");
+                this.id = m;
+                Console.WriteLine(m);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("      [ERROR] Maybe the show has another name or the episode is not there. Skipping.");
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        public void SetCurrentEpisode(string episode)
+        {
+            Match match = Regex.Match(episode, @"S..E..", RegexOptions.IgnoreCase);
+            string m = match.Groups[0].Value;
+            m = m.ToUpper();
+            m = m.Replace("S0", "").Replace("S", "").Replace("E", "x");
+            this.currentEspisode = m;
+        }
+
+        public void SetShow(string folder)
+        {
+            this.show = folder.Replace(" ", "-").ToLower();
+        }
+
+        public void GetSub(int version)
+        {
+            if (version < 5)
+            {
+                WebClient w = new WebClient();
+                w.Headers.Add("Referer", "http://www.subtitulos.es/" + this.show + "/" + this.currentEspisode);
+                Console.Write("Downloading " + this.show + " episode " + this.currentEspisode);
+                try
+                {
+                    w.DownloadFile("http://www.subtitulos.es/updated/5/" + this.id + "/" + version.ToString(), this.localLocation);
+                    FileInfo f = new FileInfo(this.localLocation);
+                    if (f.Length > 0)
+                    {
+                        Console.WriteLine("  [OK]");
+                    }
+                    else
+                    {
+                        Console.WriteLine("      [FAILED] Version " + version + " not found. Retrying with another version.");
+                        version++;
+                        this.GetSub(version);
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("       [FAILED] Version " + version + " not found. Retrying with another version.");
+                    version++;
+                    this.GetSub(version);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Too many tries. Skipped.");
+            }
+        }
+
+        public void SetLocalLocation(string location, string file)
+        {
+            file = file.Replace(".mkv", ".srt");
+            file = file.Replace(".avi", ".srt");
+            this.localLocation = location + @"\" + file;
+        }
+    }
+}
---
+++ Subtitulos/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+using Subtitulos.Classes;
+
+namespace Subtitulos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] rutasMovies = System.Configuration.ConfigurationManager.AppSettings["pathMovies"].Split(',');
+            string[] rutasShows = System.Configuration.ConfigurationManager.AppSettings["pathShows"].Split(',');
+            string[] exts = System.Configuration.ConfigurationManager.AppSettings["ext"].Split(',');
+
+            Buscar(rutasMovies, rutasShows, exts);
+        }
+
+        #region Methods
+
+        public static void Buscar(string[] pathMovies, string[] pathShows, string[] exts)
+        {
+            BuscarPeliculas(pathMovies, exts);
+            BuscarSeries(pathShows, exts);
+        }
+        
+        public static void BuscarPeliculas(string [] path,string [] exts)
+        {
+            Console.WriteLine("Movies"); 
+            foreach (string ext in exts)
+            {
+                //Console.WriteLine("Buscando peliculas en " + ext);
+                foreach (string camino in path)
+                {
+                    Console.WriteLine("   in " + camino);
+                    DirectoryInfo dir = new DirectoryInfo(camino);
+                    foreach (DirectoryInfo dir2 in dir.GetDirectories())
+                    {
+                        //Console.WriteLine(dir2);
+                        foreach (FileInfo file in dir2.GetFiles("*." + ext))
+                        {
+                            if (File.Exists(Path.ChangeExtension(file.FullName, ".srt")))
+                            {
+                                /* Nothing to do */
+                            }
+                            else
+                            {
+                                Console.WriteLine("{0}", file.Name);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        public static void BuscarSeries(string [] path, string [] exts)
+        {
+            Console.WriteLine("TV Shows");
+            foreach (string ext in exts)
+            {
+                //Console.WriteLine("Searching for " + ext);
+                foreach (string camino in path)
+                {
+                    Console.WriteLine("   in " + camino);
+                    DirectoryInfo dir = new System.IO.DirectoryInfo(camino);
+                    foreach (DirectoryInfo dir2 in dir.GetDirectories())
+                    {
+                        Console.WriteLine("      " + dir2.Name);
+                        foreach (DirectoryInfo dir3 in dir2.GetDirectories())
+                        {
+                            foreach (FileInfo file in dir3.GetFiles("*." + ext))
+                            {
+
+                                if (File.Exists(Path.ChangeExtension(file.FullName, ".srt")))
+                                {
+                                    /* Nothing to do */
+                                }
+                                else
+                                {
+                                    Console.WriteLine("{0} has no subtitles.", file.Name);
+                                    BajarSubtitulo(dir2.Name, file.Name, dir3.FullName);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public static void BajarSubtitulo(string directory, string file, string location)
+        {
+            Subtitulo sub = new Subtitulo();
+
+            sub.SetShow(directory);
+            sub.SetCurrentEpisode(file);
+            if (sub.SetId())
+            {
+                sub.SetLocalLocation(location, file);
+                sub.GetSub(0);
+            }
+        }
+
+        #endregion
+
+     }
+}
+
